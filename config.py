518ee8a768afd63f02106a722477049a45a29203
kervin27 <kervin.baquillas@gmail.com>
2025-11-06 12:37:29 +0100

    feature: adeguamento putamenti host per railway

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 
 # config.py
 DB_CONFIG = {
-    'host': 'localhost',
+    'host': 'mysql.railway.internal',
     'user': 'root',            # il tuo utente MySQL
-    'password': 'Sbiluna2024!',  # la password che usi in Workbench
-    'database': 'progetto_db',   # il nome del tuo database
+    'password': 'ZmsTdyulFNcmvtoCTHWHhQndsWvutlos',  # la password che usi in Workbench
+    'database': 'railway',   # il nome del tuo database
     'port': 3306                 # la porta (di default 3306)
 }
