915336a9c199df8491a7c773de64199f56b0573f
kervin27 <kervin.baquillas@gmail.com>
2025-11-06 17:11:12 +0100

    abilitazione per le chiamate CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from db import create_table, aggiungi_utente, leggi_utenti, aggiorna_utente, elimina_utente
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 create_table()
 
