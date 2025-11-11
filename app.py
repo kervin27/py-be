ed9385bffb77275ba0a2b5753e6c2a13813bb46b
kervin27 <kervin.baquillas@gmail.com>
2025-11-11 12:08:28 +0100

    refactor: adeguamento app.py per caricamento pagina

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, request, jsonify  # importa Flask per l'app, request per leggere richieste e jsonify per risposte JSON
 from flask_cors import CORS  # importa CORS per abilitare le richieste cross-origin
-from db import create_table, aggiungi_utente, leggi_utenti, aggiorna_utente, elimina_utente  # importa funzioni per gestire il DB
-import os  # importa il modulo os per leggere variabili d'ambiente
+import os
+from src.api.user_routes import user_bp
+from src.services.user_services import create_table  # importa il modulo os per leggere variabili d'ambiente
+from flasgger import Swagger
+
 
 app = Flask(__name__)  # crea l'istanza dell'app Flask usando il nome del modulo corrente
 CORS(app)  # abilita CORS sull'app per permettere chiamate da browser di altri domini
 
+
+# registrazione diretta dell'endpoint
 create_table()  # crea la tabella nel DB all'avvio se non esiste
+
+# registra blueprint
+app.register_blueprint(user_bp)
+
+
+swagger = Swagger(app)
+
 
 @app.route("/")  # definisce la route radice dell'API (GET di default)
 def home():  # funzione che gestisce la route radice
